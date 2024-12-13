8434983b87c3c65f9cabcc3bcf468c441f49915c
Vinay Sai Chelupuri <vinaysaichelupuri@VinaySais-MacBook.local>
2024-12-13 16:27:27 +0530

    create the register functionality and testing

--- routes/userRegister.ts
+++ routes/userRegister.ts
@@ -2,23 +2,21 @@ import express, { Request, Response } from 'express';
 import { UserService } from '../services/userSevices';
 
 const router = express.Router(); 
-router.post('/user', async (req: Request, res: Response) => {
+router.post('/users', async (req: Request, res: Response) => {
   try {
-    const { name, password } = req.body;
-    const user = await UserService.loginUser(name, password);
+    const { name, password,email,phoneNumber } = req.body;
+    const user = await UserService.registerUser(name, password,email,phoneNumber);
     if (user) {
     if(user.status===200){
-        res.status(user.status).json({message: 'Login successful'});
+        res.status(user.status).json({message: 'Register successful'});
     }
-    if(user.status===401){
-        res.status(user.status).json({message: 'Wrong password'});
-    }
-    if(user.status===404){
-        res.status(user.status).json({message: 'No user found'});
+    if(user.status===400){
+        res.status(user.status).json({message: 'Already user exists'});
     }
 }
   } catch (error) {
     res.status(500).json({ error: 'An error occurred' });
+    console.log(error)
   }
 });
 export default router;
