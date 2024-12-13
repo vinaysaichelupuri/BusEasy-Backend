43d6dc4e2019ef3fe1b869bc085a9c093ab953a4
Vinay Sai Chelupuri <vinaysaichelupuri@VinaySais-MacBook.local>
2024-12-13 16:27:47 +0530

    create the register and add route in index file

--- index.ts
+++ index.ts
@@ -1,24 +1,28 @@
 import express from 'express';
 import { sequelizeConnection } from './databaseConnections/sequelizeDatabaseConnection';
-
-import userRegister from './routes/userRegister';
+import userLogin from './routes/userLogin';
+import userRegister from './routes/userRegister'
 export const router = express.Router();
 const port = 5005;
 const app = express()
 app.use(express.json());
 const cors = require('cors');
 app.use(cors());
+import { insertBusData } from './staticData/busesData';
 
 
 async function Server(){
     try{
     await sequelizeConnection.authenticate();
+    await sequelizeConnection.sync();
     console.log("Connection has been established successfully");
 }catch(error){
     console.log("Cannot connect to database", error);
 }
 }
 Server();
+// insertBusData()
+app.use('/api',userLogin);
 app.use('/api',userRegister);
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
